5cfeb12ca1ad44e0d3c3110cf7a2592b07d25ed0
itc-n26004 <n26004@std.it-college.ac.jp>
2026-06-09 07:58:13 +0000

    第5章　例題14まで終了

---
+++ Chapter05/ex5-03.py
@@ -0,0 +1,5 @@
+ifruit = ["Apple", "Orange", "Pear"]
+fruit.append("Banana")
+fruit.append("Peach")
+print(fruit)
+
---
+++ Chapter05/ex5-08.py
@@ -0,0 +1,6 @@
+colors = ["blue", "green", "yellow"]
+print(colors)
+item = colors.pop(2)
+print(item)
+print(colors)
+
