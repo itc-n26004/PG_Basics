4b1160e62a3bbc0510198c810d2575c82327dcc0
itc-n26004 <n26004@std.it-college.ac.jp>
2026-06-17 07:55:56 +0000

    第6章 チャレンジ問題3まで

---
+++ Chapter6/challenge6-2.py
@@ -0,0 +1,4 @@
+
+name1 = "小野"
+name2 = "ミチオ"
+print("私はは昨日{}を書いて{}に送った！".format(name1, name2))
---
+++ Chapter6/challenge6-3.py
@@ -0,0 +1,7 @@
+text = "aldous Huxley was born in 1894."
+
+# 単語の先頭をすべて大文字にする
+print(text.title()) 
+# 出力: Aldous Huxley Was Born In 1894.
+
+
